326e55bb0945bac04bd637358e44ebdbd6825525
Nikita <ezio13@yandex.ru>
2022-09-22 18:14:11 +0300

    сделал задачи 10 и 13

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿
+Console.WriteLine("Введите номер задачи");
+int exerciseNum = int.Parse(Console.ReadLine());
+if (exerciseNum == 10)
+{   Console.WriteLine("Введите трехзначное число");
+    string num = Console.ReadLine();
+    char[] numArr = num.ToCharArray(0, num.Length);
+
+    if (num.StartsWith("-") & num.Length >= 4)
+    {
+        Console.WriteLine(numArr[2]);
+    }
+    else if (num.Length >= 3)
+    {
+        Console.WriteLine(numArr[1]);
+    }
+    else
+    {
+        Console.WriteLine("Число не соответствует условию");
+    }
+}
+else if (exerciseNum == 13)
+{
+    Console.WriteLine("Введите число");
+    string number = Console.ReadLine();
+    if (number.StartsWith("-"))
+    {
+        if (number.Length < 4)
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+        else{
+            Console.WriteLine(number[3]);
+        }
+    }
+    else if (number.Length < 3)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine(number[2]);
+    }
+}
